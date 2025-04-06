e3b7ef116a295b68723d3a1246387ec324405a6a
Nguyen Tien Khoa <khoa.nguyentien@hcmut.edu.vn>
2025-04-06 12:20:02 +0700

    page 4

--- page4.py
+++ page4.py
@@ -7,11 +7,16 @@ import seaborn as sns
 from xgboost import XGBClassifier
 from sqlalchemy import create_engine
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
-# Kết nối đến database Neon
+load_dotenv()
+connection_string = os.getenv('DATABASE_URL')
+
+# Kết nối đến database trên Neon
 @st.cache_resource
 def connect_db():
-    engine = create_engine("postgresql://hospital_owner:npg_J16pOhzLtlIe@ep-purple-haze-a1ykx9rh-pooler.ap-southeast-1.aws.neon.tech/hospital?sslmode=require")
+    engine = create_engine(connection_string)
     return engine
 
 # Load dữ liệu từ các bảng
